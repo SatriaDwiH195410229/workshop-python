7876dda5f49941aa1b15963dfc8da7a2f923ef0e
Satria Dwi Hartanto <77047513+SatriaDwiH195410229@users.noreply.github.com>
2022-04-19 10:28:45 +0700

    Delete src directory

--- src/12.3-1.py
+++
@@ -1,6 +0,0 @@
-(tutorial-env) $ python -m pip install novas
-Collecting novas
-  Downloading novas-3.1.1.3.tar.gz (136kB)
-Installing collected packages: novas
-  Running setup.py install for novas
-Successfully installed novas-3.1.1.3
--- src/12.3-6.py
+++
@@ -1,5 +0,0 @@
-(tutorial-env) $ pip freeze > requirements.txt
-(tutorial-env) $ cat requirements.txt
-novas==3.1.1.3
-numpy==1.9.2
-requests==2.7.0
